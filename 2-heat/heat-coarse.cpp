d8b757b4b76718c02308ac17ffb87a1f8835b06e
Yu-Hang Tang <Yuhang_Tang@brown.edu>
2016-06-18 10:57:05 -0400

    added coarse-fine finite difference coupling code

---
+++ 2-heat/heat-coarse.cpp
@@ -0,0 +1,70 @@
+/*
+ * heat-coarse.cpp
+ *
+ *  Created on: Jun 16, 2016
+ *      Author: ytang
+ */
+
+
+#include "../mui/mui.h"
+#include <algorithm>
+#include <fstream>
+
+/* Grid scheme, 4:1 coarse-fine ratio, PBC
+ * Fine   :                         o-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-o
+ *                                  0 1 2 3 4 5 6 7 8 9       ~~~          20
+ * Coarse : +-------+-------+-------+-------o-------.-------.-------o-------+-------+-------+-------+
+ *          0       1       2       3       4       5       6       7       8       9      10      11
+ * +: grid points
+ * o: interface points
+ * .: place holder for illustrative purposes
+ */
+
+int main( int argc, char ** argv ) {
+    using namespace mui;
+
+    const static int N = 12;
+	double u1[N], u2[N]; // note that it is not necessary to allocate space for node 5 & 6, but here I do it anyway to simplify coding
+    for ( int i = 0; i <  4; i++ ) u1[i] = 0;
+    for ( int i = 8; i < 12; i++ ) u1[i] = 0;
+
+    uniface1d interface( "mpi://coarse/ifs" );
+
+    double k = 0.01, H = 1, h = 0.25; // H/h : grid stride for the coarse/fine grid
+    double * u = u1, *v = u2;
+    std::ofstream fout( "solution-coarse.txt" );
+
+    fout << "TIMESTEP 0" << std::endl;
+    for ( int i = 0; i <  4; i++ ) fout << i * H << '\t' << u[i] << '\n';
+    for ( int i = 8; i < 12; i++ ) fout << i * H << '\t' << u[i] << '\n';
+
+    for ( int t = 1; t <= 100; t ++ ) {
+    	//printf("Coarse grid step %d\n", t);
+
+    	interface.push( "u", 3 * H, u[3] );
+    	interface.push( "u", 8 * H, u[8] );
+    	interface.commit( t );
+
+    	sampler_gauss1d<double> s1( 1, 0.25 );
+    	chrono_sampler_exact1d  s2;
+    	u[4] = interface.fetch( "u", 4 * H, t, s1, s2 );
+    	u[7] = interface.fetch( "u", 7 * H, t, s1, s2 );
+    	printf("coarse [4] %lf [7] %lf\n", u[4], u[7]);
+
+    	// 'interior' points
+    	for(int i=1;i< 4;i++) v[i] = u[i] + k / ( H * H ) * ( u[i - 1] + u[i + 1] - 2 * u[i] );
+    	for(int i=8;i<11;i++) v[i] = u[i] + k / ( H * H ) * ( u[i - 1] + u[i + 1] - 2 * u[i] );
+    	// 'boundary' points
+        v[    0] = u[    0] + k / ( H * H ) * ( u[1] + u[N - 1] - 2 * u[    0] );
+        v[N - 1] = u[N - 1] + k / ( H * H ) * ( u[0] + u[N - 2] - 2 * u[N - 1] );
+
+        std::swap( u, v );
+        fout << "TIMESTEP " << t << std::endl;
+        for ( int i = 0; i <  4; i++ ) fout << i * H << '\t' << u[i] << '\n';
+        for ( int i = 8; i < 12; i++ ) fout << i * H << '\t' << u[i] << '\n';
+    }
+    fout.close();
+
+    return 0;
+}
+
---
+++ 2-heat/heat-fine.cpp
@@ -0,0 +1,66 @@
+/*
+ * heat-fine.cpp
+ *
+ *  Created on: Jun 16, 2016
+ *      Author: ytang
+ */
+
+
+#include "../mui/mui.h"
+#include <algorithm>
+#include <fstream>
+
+/* Grid scheme, 4:1 coarse-fine ratio, PBC
+ * Fine   :                         o-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-o
+ *                                  0 1 2 3 4 5 6 7 8 9       ~~~          20
+ * Coarse : +-------+-------+-------+-------o-------.-------.-------o-------+-------+-------+-------+
+ *          0       1       2       3       4       5       6       7       8       9      10      11
+ * +: grid points
+ * o: interface points
+ * .: place holder for illustrative purposes
+ */
+
+int main( int argc, char ** argv ) {
+    using namespace mui;
+
+    const static int N = 21;
+	double u1[N], u2[N]; // note that it is not necessary to allocate space for node 0 & 20, but here I do it anyway to simplify coding
+    for ( int i = 1; i < 20; i++ ) u1[i] = 2 - i % 2; // spiky I.C.
+
+    uniface1d interface( "mpi://fine/ifs" );
+
+    double k = 0.01, H = 1, h = 0.25; // H/h : grid stride for the coarse/fine grid
+    double * u = u1, *v = u2;
+    std::ofstream fout( "solution-fine.txt" );
+
+    fout << "TIMESTEP 0" << std::endl;
+    for ( int i = 1; i < 20; i++ ) fout << i * h + 3 * H << '\t' << u[i] << '\n';
+
+    for ( int t = 1; t <= 100; t ++ ) {
+    	printf("Fine grid step %d\n", t);
+
+    	for(int i= 1;i< 8;i++) {
+//    		printf("pushed %lf @ %lf\n", )
+    		interface.push( "u",  i * h + 3 * H, u[i] );
+    	}
+    	for(int i=13;i<20;i++) interface.push( "u", i * h + 3 * H, u[i] );
+    	interface.commit( t );
+
+    	sampler_exact1d<double> s1;
+    	chrono_sampler_exact1d  s2;
+    	u[ 0] = interface.fetch( "u",  0 * h + 3 * H, t, s1, s2 );
+    	u[20] = interface.fetch( "u", 20 * h + 3 * H, t, s1, s2 );
+    	printf("fine [0] %lf [20] %lf\n", u[0], u[20]);
+
+    	// all points are 'interior'
+    	for(int i=1;i<20;i++) v[i] = u[i] + k / ( h * h ) * ( u[i - 1] + u[i + 1] - 2 * u[i] );
+
+        std::swap( u, v );
+        fout << "TIMESTEP " << t << std::endl;
+        for ( int i = 1; i < 20; i++ ) fout << i * h + 3 * H << '\t' << u[i] << '\n';
+    }
+    fout.close();
+
+    return 0;
+}
+
