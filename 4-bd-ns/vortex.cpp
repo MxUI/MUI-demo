f5c1a752ff203813ecf5e3869c03e943cd688352
Stephen Longshaw <slongshaw@gmail.com>
2019-07-04 17:11:50 +0100

    Update license boilerplates

--- 4-bd-ns/vortex.cpp
+++ 4-bd-ns/vortex.cpp
@@ -1,21 +1,52 @@
-/*
- * vortex.cpp
+/*****************************************************************************
+* Multiscale Universal Interface Code Coupling Library Demo 4                *
+*                                                                            *
+* Copyright (C) 2019 Y. H. Tang, S. Kudo, X. Bian, Z. Li, G. E. Karniadakis  *
+*                                                                            *
+* This software is jointly licensed under the Apache License, Version 2.0    *
+* and the GNU General Public License version 3, you may use it according     *
+* to either.                                                                 *
+*                                                                            *
+* ** Apache License, version 2.0 **                                          *
+*                                                                            *
+* Licensed under the Apache License, Version 2.0 (the "License");            *
+* you may not use this file except in compliance with the License.           *
+* You may obtain a copy of the License at                                    *
+*                                                                            *
+* http://www.apache.org/licenses/LICENSE-2.0                                 *
+*                                                                            *
+* Unless required by applicable law or agreed to in writing, software        *
+* distributed under the License is distributed on an "AS IS" BASIS,          *
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   *
+* See the License for the specific language governing permissions and        *
+* limitations under the License.                                             *
+*                                                                            *
+* ** GNU General Public License, version 3 **                                *
+*                                                                            *
+* This program is free software: you can redistribute it and/or modify       *
+* it under the terms of the GNU General Public License as published by       *
+* the Free Software Foundation, either version 3 of the License, or          *
+* (at your option) any later version.                                        *
+*                                                                            *
+* This program is distributed in the hope that it will be useful,            *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+* GNU General Public License for more details.                               *
+*                                                                            *
+* You should have received a copy of the GNU General Public License          *
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.      *
+******************************************************************************/
+
+/**
+ * @file vortex.cpp
+ * @author Y. H. Tang
+ * @date 9 July 2016
+ * @brief Flow with a stagnation point in a square box.
  *
- *  Created on: Jul 9, 2016
- *      Author: ytang
- */
-
-#include "../mui/mui.h"
-#include <algorithm>
-#include <fstream>
-
-// USAGE: mpirun -np 1 ./brownian : -np 4 ./vortex
-
-/* flow with a stagnation point in a square box
- * Yazdani, A., Deng, M., Caswell, B., & Karniadakis, G. E. (2016). Flow in complex domains simulated by Dissipative
- * Particle Dynamics driven by geometry-specific body-forces. Journal of Computational Physics, 305, 906-920.
- *
- * USAGE: mpirun -np 1 ./brownian : -np 4 ./vortex
+ * Yazdani, A., Deng, M., Caswell, B., & Karniadakis, G. E. (2016).
+ * Flow in complex domains simulated by Dissipative Particle Dynamics
+ * driven by geometry-specific body-forces. Journal of Computational Physics,
+ * 305, 906-920.
  *
  *                 1
  *     -------------------------
@@ -37,7 +68,13 @@
  *
  * X: stagnation point
  * o: particle initial position
+ *
+ * USAGE: mpirun -np 1 ./brownian : -np 4 ./vortex
  */
+
+#include "../mui/mui.h"
+#include <algorithm>
+#include <fstream>
 
 int main( int argc, char ** argv ) {
     using namespace mui;
